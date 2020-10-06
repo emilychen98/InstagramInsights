981ab8873e5b2c547acf444e6d413bcd31186c1d
Andy Liu <ala158@sfu.ca>
2020-10-05 22:19:24 -0700

    backend files

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+// var mongoose = require('mongoose');
+// var bodyparser = require('body-parser');
+// var cors = require('cors');
+// var path = require('path');
+
+var app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('yes');
+});
+
+app.listen(port, () =>  {
+    console.log("hi")
+});
